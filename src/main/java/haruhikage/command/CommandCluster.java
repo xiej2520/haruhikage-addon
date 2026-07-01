ca6e6a22498e05d754289daec24ca68ef21a7132
xiej2520 <xiej2520@gmail.com>
2026-06-30 22:04:42 -0500

    RoyanAB: improve cluster calculator
    Use thread pool, fix bugs. Should be O(log n)
    
    Author: RoyanAB

--- src/main/java/haruhikage/command/CommandCluster.java
+++ src/main/java/haruhikage/command/CommandCluster.java
@@ -1,7 +1,9 @@
 package carpet.commands;
 
 import carpet.CarpetServer;
-import carpet.helpers.ClusterHelper;
+import carpet.ClusterHelper;
+import carpet.ClusterUtil;
+import carpet.MSTGeneration;
 import carpet.utils.Messenger;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
@@ -14,14 +16,16 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.HttpUtil;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 import java.util.concurrent.Executors;
 
 public class CommandCluster extends CommandCarpetBase {
-    private ClusterHelper clusterHelper = new ClusterHelper();
+
+    public ClusterHelper.ClusterData tempData = null;
 
     @Override
     public String getName() {
@@ -35,7 +39,7 @@ public class CommandCluster extends CommandCarpetBase {
 
     @Override
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
-        if (args.length == 0) {
+        if (args.length == 1) {
             return getListOfStringsMatchingLastWord(args, "peek", "read", "compute", "loadCluster", "loadGrid", "constructLoader");
         }
         return Collections.emptyList();
@@ -48,13 +52,13 @@ public class CommandCluster extends CommandCarpetBase {
         if (args.length < 1) return;
         switch (args[0]) {
             case "peek":
-                Messenger.s(sender, clusterHelper.writeToNBT(new NBTTagCompound()).toString());
+                Messenger.s(sender, tempData.writeToNBT(new NBTTagCompound()).toString());
                 break;
             case "read":
                 if (args.length < 2) return;
                 try {
                     NBTTagCompound compound = JsonToNBT.getTagFromJson(args[1]);
-                    clusterHelper.readFromNBT(compound);
+                    tempData = new ClusterHelper.ClusterData(compound);
                     Messenger.s(sender, "Data successfully read! ");
                 } catch (NBTException e) {
                     throw new CommandException("Malformed NBT");
@@ -63,39 +67,44 @@ public class CommandCluster extends CommandCarpetBase {
             case "compute":
                 HttpUtil.DOWNLOADER_EXECUTOR.submit(() -> {
                     long startMs = System.currentTimeMillis();
-                    int value = clusterHelper.compute();
-                    CarpetServer.minecraft_server.callFromMainThread(Executors.callable(() -> {
-                        Messenger.s(sender, "Computation of optimal cluster chunks completed in " +
-                            (System.currentTimeMillis() - startMs) + " milliseconds, and the grid has " + value + " chunks");
-                        Messenger.s(sender, "The optimal target chunk is " + clusterHelper.optimalTargetChunk);
-                        Messenger.s(sender, "The optimal 4 chunks to exclude is " + clusterHelper.optimalExcludedRelative);
-                    }));
+                    ClusterHelper.ClusterData computedData = ClusterHelper.compute(tempData);
+                    if (computedData == null) {
+                        CarpetServer.minecraft_server.callFromMainThread(Executors.callable(() -> {
+                            Messenger.s(sender, "Calculation failed");
+                        }));
+                    } else {
+                        CarpetServer.minecraft_server.callFromMainThread(Executors.callable(() -> {
+                            Messenger.s(sender, "Computation of optimal cluster chunks completed in " +
+                                (System.currentTimeMillis() - startMs) + " milliseconds, and the grid has " + computedData.loadingGridSize + " chunks");
+                            Messenger.s(sender, "The optimal target chunk is " + computedData.targetChunk);
+                        }));
+                    }
                 });
                 Messenger.s(sender, "Cluster computation started on an async thread. Please wait patiently. ");
                 break;
             case "loadCluster":
-                clusterHelper.loadCluster(sender.getEntityWorld());
+                ClusterHelper.tempData.clusterChunks.forEach(chunkPos -> {
+                    sender.getEntityWorld().getChunk(chunkPos.x, chunkPos.z);
+                });
                 Messenger.s(sender, "Cluster chunks loaded! ");
                 break;
             case "loadGrid":
-                clusterHelper.loadGrid(sender.getEntityWorld());
+                Set<ChunkPos> structureMap = MSTGeneration.generateStructureMap(ClusterHelper.tempData.clusterCornerPos, ClusterHelper.tempData.MSTEdgeList, ClusterHelper.tempData.clusterChunks);
+                structureMap.forEach(chunkPos -> {
+                    sender.getEntityWorld().getChunk(chunkPos.x, chunkPos.z);
+                });
                 Messenger.s(sender, "Loading grid loaded! ");
                 break;
             case "constructLoader":
                 int y = 64;
-                if (args.length < 3) {
-                    Messenger.s(sender, "Simulating the construction of the loader");
-                    int disconnections = clusterHelper.checkClusterLoader();
-                    Messenger.s(sender, "A total of " + disconnections + " chunks are disconnected! ");
-                    return;
-                }
                 Block block = CommandBase.getBlockByText(sender, args[1]);
                 int metadata = Integer.parseInt(args[2]);
                 IBlockState buildingBlock = block.getStateFromMeta(metadata);
                 if (args.length >= 4) y = Integer.parseInt(args[3]);
-                clusterHelper.buildClusterLoader(sender.getEntityWorld(), y, buildingBlock);
+                MSTGeneration.buildLoadingGrid(ClusterHelper.tempData.clusterCornerPos, ClusterHelper.tempData.MSTEdgeList, ClusterHelper.tempData.clusterChunks, sender.getEntityWorld(), y, buildingBlock);
                 Messenger.s(sender, "Loader for the cluster grid has been constructed!!! ");
                 break;
         }
     }
 }
+
